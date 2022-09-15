5cfcb8175e304da8553027c84aa8943d7d1fae5e
ydicoder <79189798+ydipeepo@users.noreply.github.com>
2022-09-15 13:59:48 +0900

    Addd shorthand methods to EaseMotionBuilder

--- addons/godot-motion/builder/EaseMotionBuilder.gd
+++ addons/godot-motion/builder/EaseMotionBuilder.gd
@@ -15,9 +15,69 @@ func set_ease(ease_type: int) -> EaseMotionBuilder:
 	_generator_init.ease_type = ease_type
 	return self
 
+func in() -> EaseMotionBuilder:
+	_generator_init.ease_type = Tween.EASE_IN
+	return self
+
+func out() -> EaseMotionBuilder:
+	_generator_init.ease_type = Tween.EASE_OUT
+	return self
+
+func in_out() -> EaseMotionBuilder:
+	_generator_init.ease_type = Tween.EASE_IN_OUT
+	return self
+
+func out_in() -> EaseMotionBuilder:
+	_generator_init.ease_type = Tween.EASE_OUT_IN
+	return self
+
 # トランジションタイプを設定します
 func set_trans(trans_type: int) -> EaseMotionBuilder:
 	_generator_init.trans_type = trans_type
+	return self
+
+func linear() -> EaseMotionBuilder:
+	_generator_init.trans_type = Tween.TRANS_LINEAR
+	return self
+
+func quad() -> EaseMotionBuilder:
+	_generator_init.trans_type = Tween.TRANS_QUAD
+	return self
+
+func cubic() -> EaseMotionBuilder:
+	_generator_init.trans_type = Tween.TRANS_CUBIC
+	return self
+
+func quart() -> EaseMotionBuilder:
+	_generator_init.trans_type = Tween.TRANS_QUART
+	return self
+
+func quint() -> EaseMotionBuilder:
+	_generator_init.trans_type = Tween.TRANS_QUINT
+	return self
+
+func sine() -> EaseMotionBuilder:
+	_generator_init.trans_type = Tween.TRANS_SINE
+	return self
+
+func expo() -> EaseMotionBuilder:
+	_generator_init.trans_type = Tween.TRANS_EXPO
+	return self
+
+func circ() -> EaseMotionBuilder:
+	_generator_init.trans_type = Tween.TRANS_CIRC
+	return self
+
+func elastic() -> EaseMotionBuilder:
+	_generator_init.trans_type = Tween.TRANS_ELASTIC
+	return self
+
+func back() -> EaseMotionBuilder:
+	_generator_init.trans_type = Tween.TRANS_BACK
+	return self
+
+func bounce() -> EaseMotionBuilder:
+	_generator_init.trans_type = Tween.TRANS_BOUNCE
 	return self
 
 # 開始までの遅延を設定します
